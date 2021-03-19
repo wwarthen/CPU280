c1466b0e093f92991e5d77cb7f11e5b419d4ee8f
Wayne Warthen <wwarthen@gmail.com>
2021-03-19 11:37:39 -0700

    Cleanup
    Updated ANSICON to latest version and removed some redundant files.

---
+++ Floppy/Clean-ZPM3.cmd
@@ -0,0 +1,21 @@
+@echo off
+setlocal
+
+if exist ZPM3-Master.img del ZPM3-Master.img
+if exist ZPM3-Files\u0\readme.txt del ZPM3-Files\u0\readme.txt
+
+if exist ZPM3-Files\u0\ccp.com del ZPM3-Files\u0\ccp.com
+if exist ZPM3-Files\u15\loadseg.com del ZPM3-Files\u15\loadseg.com
+if exist ZPM3-Files\u0\zinstal.zpm del ZPM3-Files\u0\zinstal.zpm
+if exist ZPM3-Files\u15\diskinfo.com del ZPM3-Files\u15\diskinfo.com
+if exist ZPM3-Files\u15\rsxdir.com del ZPM3-Files\u15\rsxdir.com
+
+if exist ZPM3-Files\u0\cpm3.sys del ZPM3-Files\u0\cpm3.sys
+if exist ZPM3-Files\u15\autotog.com del ZPM3-Files\u15\autotog.com
+if exist ZPM3-Files\u15\clrhist.com del ZPM3-Files\u15\clrhist.com
+if exist ZPM3-Files\u15\setz3.com del ZPM3-Files\u15\setz3.com
+
+if exist ZPM3-Files\u15\format.com del ZPM3-Files\u15\format.com
+if exist ZPM3-Files\u15\format.dat del ZPM3-Files\u15\format.dat
+
+if exist ZPM3-Files\u15\idetest.com del ZPM3-Files\u15\idetest.com
